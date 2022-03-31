04182c4b4ca920a972af515a0bdff47efbb5b2e8
Fappablo <thefappachannelita@gmail.com>
2022-03-31 17:46:29 +0200

    cleanup, optimized register function

--- contracts/SubdomainStore.sol
+++ contracts/SubdomainStore.sol
@@ -25,17 +25,22 @@ contract SubdomainStore is IERC721Receiver, Ownable, ApproveAndCallFallBack {
     uint price;
   }
 
-  bytes32 constant public TLD_NODE = 0x93cdeb708b7545dc668eb9280176169d1c33cfd8ed6f04690a0bcc88a93fc4ae;
+  bytes32 constant internal TLD_NODE = 0x93cdeb708b7545dc668eb9280176169d1c33cfd8ed6f04690a0bcc88a93fc4ae;
   address internal xbtc = 0x244aA29426fb6524760bD9AcbB66ad53C5EB32CA;
-
-  ENS public ens = ENS(0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e);
-  mapping (bytes32 => Domain) public domains;
+  ENS internal ens = ENS(0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e);
+  
+  mapping (bytes32 => Domain) internal domains;
   
   constructor() {
     
   }
   
-  function configureDomainFor(string memory name, uint price) public onlyOwner {
+  /**
+   * @dev Configures a domain for sale.
+   * @param name The name to configure.
+   * @param price The price in wei to charge for subdomain registrations
+   */
+  function configureDomain(string memory name, uint price) public onlyOwner {
     bytes32 label = keccak256(bytes(name));
     Domain storage domain = domains[label];
 
@@ -47,15 +52,6 @@ contract SubdomainStore is IERC721Receiver, Ownable, ApproveAndCallFallBack {
     domain.price = price;
   }
 
-  /**
-   * @dev Configures a domain for sale.
-   * @param name The name to configure.
-   * @param price The price in wei to charge for subdomain registrations
-   */
-  function configureDomain(string memory name, uint price) public onlyOwner {
-    configureDomainFor(name, price);
-  }
-
   function setResolver(string memory name, address resolver) public onlyOwner {
     bytes32 label = keccak256(bytes(name));
     bytes32 node = keccak256(abi.encodePacked(TLD_NODE, label));
@@ -63,39 +59,29 @@ contract SubdomainStore is IERC721Receiver, Ownable, ApproveAndCallFallBack {
   }
   
   function doRegistration(bytes32 node, bytes32 label, address subdomainOwner, Resolver resolver) internal {
-    // Get the subdomain so we can configure it
-    ens.setSubnodeOwner(node, label, address(this));
-    bytes32 subnode = keccak256(abi.encodePacked(node, label));
-    // Set the subdomain's resolver
-    ens.setResolver(subnode, address(resolver));
-    // Set the address record on the resolver
-    resolver.setAddr(subnode, subdomainOwner);
-    // Pass ownership of the new subdomain to the registrant
-    ens.setOwner(subnode, subdomainOwner);
-  } 
+    ens.setSubnodeRecord(node, label, subdomainOwner, address(resolver), 0);
+  }
 
   /**
    * @dev Registers a subdomain.
    * @param label The label hash of the domain to register a subdomain of.
    * @param subdomain The desired subdomain label.
-   * @param _subdomainOwner The account that should own the newly configured subdomain.
+   * @param subdomainOwner The account that should own the newly configured subdomain.
    */
-  function register(bytes32 label, string memory subdomain, address _subdomainOwner, address resolver) public {
-    address subdomainOwner = _subdomainOwner;
-
+  function register(bytes32 label, string memory subdomain, address subdomainOwner, address resolver) public {
     bytes32 domainNode = keccak256(abi.encodePacked(TLD_NODE, label));
 
     bytes32 subdomainLabel = keccak256(bytes(subdomain));
 
     // Subdomain must not be registered already.
-    require(ens.owner(keccak256(abi.encodePacked(domainNode, subdomainLabel))) == address(0),"Subdomain must not be registered already");
+    require(ens.owner(keccak256(abi.encodePacked(domainNode, subdomainLabel))) == address(0), "Subdomain must not be registered already");
     Domain storage domain = domains[label];
 
     // Domain must be available for registration
-    require(keccak256(abi.encodePacked(domain.name)) == label,"Domain must be available for registration");
+    require(keccak256(abi.encodePacked(domain.name)) == label, "Domain must be available for registration");
 
-    // User must have paid
-    require(IERC20(xbtc).transferFrom(subdomainOwner, address(this), domain.price),"User must have paid");
+    // The account that gets the subdomain also needs to pay
+    require(IERC20(xbtc).transferFrom(subdomainOwner, address(this), domain.price), "User must have paid");
 
     doRegistration(domainNode, subdomainLabel, subdomainOwner, Resolver(resolver));
   }
@@ -116,8 +102,14 @@ contract SubdomainStore is IERC721Receiver, Ownable, ApproveAndCallFallBack {
     register(label, subdomain, from, resolver);
   }
 
-  function encodeHelper(bytes32 label, string calldata subdomain, address resolver) external pure returns (bytes memory data) {
+  //Helper function to encode the data needed for ApproveAndCall
+  function encodeData(bytes32 label, string calldata subdomain, address resolver) external pure returns (bytes memory data) {
     return abi.encode(label, subdomain, resolver);
+  }
+
+  //Helper function to get the label hash
+  function encodeLabel(string calldata label) external pure returns (bytes32 encodedLabel) {
+    return keccak256(bytes(label));
   }
 
   function getPrice (bytes32 label) external view returns (uint256 price){
